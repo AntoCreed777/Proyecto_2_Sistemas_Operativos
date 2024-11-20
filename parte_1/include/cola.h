463628498e1ea8fcb59ad315a5dbae323cd7048d
karantooo <ctomasalvarezn@gmail.com>
2024-11-20 19:32:52 -0300

    fix: se resuelve elemento inicial que no existe en la cola cuando se inicializa por tamanio

--- parte_1/include/cola.h
+++ parte_1/include/cola.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -15,13 +16,15 @@ private:
     int head;
     int size_cola;
 
+    std::string ruta_default_log = "log.txt";
+
     void aumentar_tamanio();
     void reducir_tamanio();
 
 public:
     Cola();
     Cola(std::vector<elemento> cola_inicial);
-    Cola(int tamanio_inicial);
+    Cola(std::size_t tamanio_inicial);
 
     void push(elemento elem);
 
@@ -55,6 +58,11 @@ Cola<elemento>::Cola(){
     tail = 0;
     size_cola = 0;
     contenedor = std::vector<elemento>(1);
+    utils::generar_log(
+        "\n\n\n\nSe inicializa la cola en 0 elementos\n\n",
+        this->ruta_default_log
+    );
+
 
 } 
 
@@ -64,15 +72,22 @@ Cola<elemento>::Cola(std::vector<elemento> cola_inicial){
     tail = 0;
     size_cola = cola_inicial.size();
     contenedor = cola_inicial;
-    utils::generar_log("Se inicializa la cola en " + utils::vector_to_string(contenedor) + "\n\n", "log.txt");
+    utils::generar_log(
+        "\n\n\n\nSe inicializa la cola en " + utils::vector_to_string(contenedor) + "\n\n",
+        this->ruta_default_log
+    );
 } 
 
 template<typename elemento>
-Cola<elemento>::Cola(int tamanio_inicial){
+Cola<elemento>::Cola(size_t tamanio_inicial){
     head = 0;
-    tail = 0;
+    tail = (tamanio_inicial > 1)? 1 : 0;
     size_cola = 0;
     contenedor = std::vector<elemento>(tamanio_inicial);
+    utils::generar_log(
+        "\n\n\n\nSe inicializa la cola en 0 elementos pero con memoria definida por defecto\n\n",
+        this->ruta_default_log
+    );
 
 }
 
@@ -89,7 +104,7 @@ void Cola<elemento>::aumentar_tamanio(){
     head = j;
     tail = 0;
     contenedor = contenedor_secundario;
-    utils::generar_log("Se a aumentado el tamanio al doble\nTamanio actual " + std::to_string(this->tamanio_contenedor()), "log.txt");
+    utils::generar_log("Se a aumentado el tamanio al doble\nTamanio actual " + std::to_string(this->tamanio_contenedor()), ruta_default_log);
 }
 
 template <typename elemento>
@@ -104,7 +119,7 @@ void Cola<elemento>::reducir_tamanio(){
     head = j;
     tail = 0;
     contenedor = contenedor_secundario;
-    utils::generar_log("Se a reducido el tamanio a la mitad\nTamanio actual " + std::to_string(this->tamanio_contenedor()), "log.txt");
+    utils::generar_log("Se a reducido el tamanio a la mitad\nTamanio actual " + std::to_string(this->tamanio_contenedor()), ruta_default_log);
 }
 
 template <typename elemento>
@@ -115,6 +130,7 @@ void Cola<elemento>::push(elemento elem){
     head = (head + 1) % this->tamanio_contenedor();
     contenedor[head] = elem;
     size_cola++;
+    utils::generar_log("Se añadio a la cola " + std::to_string(elem), this->ruta_default_log);
 }
 
 
@@ -133,6 +149,9 @@ elemento Cola<elemento>::pop(){
     elemento elem = contenedor[this->tail];
     this->tail = (this->tail + 1) % this->tamanio_contenedor();
     size_cola--;
+    utils::generar_log("Se elimino de la cola " + std::to_string(elem), this->ruta_default_log);
+
+
     return elem;
 }
 
