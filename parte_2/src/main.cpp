181e2b08d0bac237871580583c158a3b233cba79
Javier Campos <camposjavier143@gmail.com>
2024-11-23 16:40:08 -0300

    feat: manejo entrada de los parametros en el main 2 con validación

--- parte_2/src/main.cpp
+++ parte_2/src/main.cpp
@@ -11,7 +11,7 @@
 void manejar_argumentos(int argc, char const *argv[], int &numero_de_marcos, std::vector<int> &referencias, std::string &algoritmo){
     for(int i = 0; i < argc; i++){
         if(std::string(argv[i]) == "-m"){                            // Numero de marcos
-            numero_de_marcos = std::atoi(argv[++i]); 
+            numero_de_marcos = std::atoi(argv[i + 1]) ? std::atoi(argv[++i]) : -1;
         }
         else if(std::string(argv[i]) == "-a"){                       // Seleccionar algoritmo
             algoritmo = std::string(argv[++i]);
@@ -33,7 +33,7 @@ void manejar_argumentos(int argc, char const *argv[], int &numero_de_marcos, std
                 archivo_referencias.close();
             }
             else 
-                std::cout << ROJO "No se pudo abrir el archivo.\n" RESET_COLOR;
+                std::cout << ROJO "No se pudo abrir el archivo.\n" RESET_COLOR << std::endl;
         }
     }
 }
@@ -52,8 +52,8 @@ int main(int argc, char const *argv[]) {
     manejar_argumentos(argc, argv, numero_de_marcos, referencias, nombre_algoritmo); // Funciona
 
     if(numero_de_marcos < 0 || nombre_algoritmo == "" || referencias.empty()){
-        std::cerr << "Argumentos erroneos" << std::endl;
-        std::cerr << "Los argumentos debe ser" << argv[0] << " -m <numero marco de páginas> -a <algoritmo de remplazo> -f <nombre del archivo con las referencias>" RESET_COLOR <<std::endl;
+        std::cerr << ROJO "Argumentos erroneos" RESET_COLOR << std::endl;
+        std::cerr << ROJO "Los argumentos debe ser" << argv[0] << " -m <numero marco de páginas> -a <algoritmo de remplazo> -f <nombre del archivo con las referencias>" RESET_COLOR <<std::endl;
         return 1;
     }
 
@@ -90,10 +90,10 @@ int main(int argc, char const *argv[]) {
         }
     }
     else {
-        std::cerr << "Algoritmo invalido" << std::endl;
-        std::cerr << "Algoritmos validos son FIFO, LRU, LRUR (Reloj simple) y Optimo" << std::endl;
+        std::cerr << ROJO "Algoritmo invalido" << std::endl;
+        std::cerr << AMARILLO "Algoritmos validos son FIFO, LRU, LRUR (Reloj simple) y Optimo" RESET_COLOR << std::endl;
         return 1;
     }
-    
+
     return 0;
 }
