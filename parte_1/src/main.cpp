eb607f6f68eaf36e49b66ddb02baa4c17c191de6
AntoCreed777 <abenavides@desem.cl>
2024-11-24 17:10:30 -0300

    fix: Correcion en validacion de argumentos e impresion de errores mas detallada

--- parte_1/src/main.cpp
+++ parte_1/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <thread>
+#include <cctype>
+#include <algorithm>
 #include "../include/monitor.h"
 #include "../include/constantes.h"
 
@@ -19,55 +21,101 @@ void consumidor(Monitor &monitor, int id) {
     monitor.mostrarElementos();
 }
 
+bool es_numero(const std::string& str) {
+    return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+}
+
+void mensaje_error_argumentos(char const *argv[]) {
+    std::cerr << ROJO << "Error: Argumentos invalidos" << RESET_COLOR << std::endl;
+    std::cerr << AZUL << "Use: "<< AMARILLO << argv[0] << " -p <cantidad_productores> -c <cantidad_consumidores> -s <tamano_inicial_cola> -t <tiempo_espera>" << std::endl;
+    exit(EXIT_FAILURE);
+}
+
 void cargar_argumentos(int argc, char const *argv[], int &cantidad_productores, int &cantidad_consumidores, int &tamano_inicial_cola, int &tiempo_espera) {
-    for (int i = 1; i < argc; i++) {
-        if (std::string(argv[i]) == "-p")
-            cantidad_productores = std::stoi(argv[i + 1]);
+    for (int i = 1; i < argc; i+=2) {
+        if (std::string(argv[i]) == "-p") {
+            if (i + 1 >= argc) {
+                std::cerr << ROJO << "Error: Falta el argumento para -p" << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
 
-        else if (std::string(argv[i]) == "-c")
-            cantidad_consumidores = std::stoi(argv[i + 1]);
+            if (es_numero(argv[i + 1])) {
+                cantidad_productores = std::stoi(argv[i + 1]);
+            }            
+            else {
+                std::cerr << ROJO << "Error: El argumento para -p debe ser un número positivo." << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
+        }
+        else if (std::string(argv[i]) == "-c") {
+            if (i + 1 >= argc) {
+                std::cerr << ROJO << "Error: Falta el argumento para -c" << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
 
-        else if (std::string(argv[i]) == "-s")
-            tamano_inicial_cola = std::stoi(argv[i + 1]);
+            if (es_numero(argv[i + 1])) {
+                cantidad_consumidores = std::stoi(argv[i + 1]);
+            }
+            else {
+                std::cerr << ROJO << "Error: El argumento para -c debe ser un número positivo." << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
+        }
+        else if (std::string(argv[i]) == "-s") {
+            if (i + 1 >= argc) {
+                std::cerr << ROJO << "Error: Falta el argumento para -s" << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
 
-        else if (std::string(argv[i]) == "-t")
-            tiempo_espera = std::stoi(argv[i + 1]);
+            if (es_numero(argv[i + 1])) {
+                tamano_inicial_cola = std::stoi(argv[i + 1]);
+            }
+            else {
+                std::cerr << ROJO << "Error: El argumento para -s debe ser un número positivo." << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
+        }
+        else if (std::string(argv[i]) == "-t") {
+            if (i + 1 >= argc) {
+                std::cerr << ROJO << "Error: Falta el argumento para -t" << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
+
+            if (es_numero(argv[i + 1])) {
+                tiempo_espera = std::stoi(argv[i + 1]);
+            }
+            else {
+                std::cerr << ROJO << "Error: El argumento para -t debe ser un número positivo." << RESET_COLOR << std::endl;
+                mensaje_error_argumentos(argv);
+            }
+        }
+        else {
+            std::cerr << ROJO << "Error: Argumento invalido" << RESET_COLOR << std::endl;
+            mensaje_error_argumentos(argv);
+        }
     }
 }
 
 int main(int argc, char const *argv[]) {
-    if (argc != 9) {
-        std::cerr << "Error: Argumentos invalidos" << std::endl;
-        std::cerr << "Use: " << argv[0] << " -p <cantidad_productores> -c <cantidad_consumidores> -s <tamano_inicial_cola> -t <tiempo_espera>" << std::endl;
-        return 1;
-    }
-
-    // Argumentos (Se inicializan en -1 para verificar que se hayan cargado)
-    int cantidad_productores = -1;
-    int cantidad_consumidores = -1;
-    int tiempo_espera = -1; // En Segundos
-    int tamanio_inicial_cola = -1;
+    if (argc != 9) 
+        mensaje_error_argumentos(argv);
+    
+    int cantidad_productores;
+    int cantidad_consumidores;
+    int tiempo_espera;      // En Segundos
+    int tamanio_inicial_cola;
 
     cargar_argumentos(argc, argv, cantidad_productores, cantidad_consumidores, tamanio_inicial_cola, tiempo_espera);
-
-    if (cantidad_productores < 0 || cantidad_consumidores < 0 || tamanio_inicial_cola < 0 || tiempo_espera < 0) {
-        std::cerr << "Error: Argumentos invalidos" << std::endl;
-        std::cerr << "Use: " << argv[0] << " -p <cantidad_productores> -c <cantidad_consumidores> -s <tamano_inicial_cola> -t <tiempo_espera>" << std::endl;
-        return 1;
-    }
     
     Monitor monitor(tamanio_inicial_cola, tiempo_espera);
 
     std::vector<std::thread> hilos;
 
-    // Productores
-    for (int i = 0; i < cantidad_productores; i++) {
+    for (int i = 0; i < cantidad_productores; i++)
         hilos.push_back(std::thread(productor, std::ref(monitor), i));
-    }
 
-    for (int i = 0; i < cantidad_consumidores; i++) {
+    for (int i = 0; i < cantidad_consumidores; i++)
         hilos.push_back(std::thread(consumidor, std::ref(monitor), i));
-    }
     
     for (auto &hilo : hilos) hilo.join();
     hilos.clear();
